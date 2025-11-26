49dee2e13ee3b59b9e4c76b82cfee1225987f47b
hamed <hamed.sepehrnia85@gmail.com>
2025-11-26 18:37:24 +0330

    fixed blog bug

--- src/components/templates/BlogDetails/BlogDetailsHeader/BlogDetailsHeader.tsx
+++ src/components/templates/BlogDetails/BlogDetailsHeader/BlogDetailsHeader.tsx
@@ -27,26 +27,36 @@ function BlogDetailsHeader({
     <div className="px-8 pt-8 ">
       <div className="flex flex-wrap items-center justify-between gap-4 mb-6">
         <ul className="flex flex-wrap gap-6 text-paragray">
-          <li>
-            <span className="bg-accent text-white px-4 py-2 rounded-[10px] text-sm font-estedad-light ">
-              {categories[0].name}
-            </span>
-          </li>
-          <li className="flex items-center gap-2 font-estedad-light">
-            <i className="far fa-user text-accent"></i>{author}
-          </li>
+          {/* نمایش کتگوری فقط اگر وجود داشته باشد */}
+          {categories && categories.length > 0 && (
+            <li>
+              <span className="bg-accent text-white px-4 py-2 rounded-[10px] text-sm font-estedad-light ">
+                {categories[0].name}
+              </span>
+            </li>
+          )}
+          
+          {author && (
+            <li className="flex items-center gap-2 font-estedad-light">
+              <i className="far fa-user text-accent"></i>
+              {author}
+            </li>
+          )}
+          
           <li className="flex items-center gap-2 font-estedad-light">
             <i className="far fa-calendar-alt text-accent"></i>
             {formatJalali(createdAt)}
           </li>
         </ul>
+        
         <ul className="flex flex-wrap gap-6 text-paragray">
           <li className="flex items-center gap-2 font-estedad-light">
-            <i className="far fa-comment"></i>۰۵
+            <i className="far fa-comment"></i>
+            ۰۵
           </li>
-    
         </ul>
       </div>
+      
       <h2 className="text-3xl font-estedad-semibold text-dark">{title}</h2>
     </div>
   );
